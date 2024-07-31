8320fb431529a1f23d129b51c73c3065381ef275
SaMuRaIchI <mongushsayin@mail.ru>
2024-07-31 22:41:22 +0700

    Add files via upload

--- syki.py
+++ syki.py
@@ -1,4 +1,4 @@
-kolwoDz = '15'
-kolwoTime = '1.5'
+kolwoDz = 15
+kolwoTime = 1.5
 name = 'Python'
-print('Курс:',name , '.Всего задач:',kolwoDz, '.Затрачено времени:', kolwoTime,'ч','.Среднее время выполнения задач:', 1.5/15,'ч.')
+print('Курс:',name , '.Всего задач:',kolwoDz, '.Затрачено времени:', kolwoTime,'ч','.Среднее время выполнения задач:',kolwoTime/kolwoDz, 'ч.')
